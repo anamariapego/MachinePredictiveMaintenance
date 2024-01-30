f8003150c72e1aa8ae1cf719b37cf183d746d319
Ana Pego <anapego0404@gmail.com>
2024-01-30 20:01:04 -0300

    add file analyticsdata.py

---
+++ analyticsdata.py
@@ -0,0 +1,9 @@
+# Imports 
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+import matplotlib 
+import sklearn
+
+# Load dataset
+dataset = pd.read_csv("./Data/predictive_maintenance.csv")
