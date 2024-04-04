9294ea2ce11a765aa1e3b5323a3646a8256cd59a
Ana Pego <anapego0404@gmail.com>
2024-04-03 22:11:49 -0300

    delete

--- analyticsdata.py
+++
@@ -1,9 +0,0 @@
-# Imports 
-import pandas as pd 
-import numpy as np 
-import seaborn as sns
-import matplotlib 
-import sklearn
-
-# Load dataset
-dataset = pd.read_csv("./Data/predictive_maintenance.csv")
